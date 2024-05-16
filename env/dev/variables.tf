37067dfda83fd87cda57592c60fa1f3f1c0eb847
khk <cjdshrwkd11@gmail.com>
2024-05-16 17:50:51 +0900

    update

--- env/dev/variables.tf
+++ env/dev/variables.tf
@@ -12,11 +12,11 @@ variable "cidr_private2"   { default = "10.0.12.0/24" }
 # Bastion
 variable "bastion_ami"           { default = "ami-0e6f2b2fa0ca704d0" }
 variable "bastion_instance_type" { default = "t2.medium" }
-variable "bastion_key_name"      { default = "./keyfair/bastion-ec2-key.pem" }
+variable "bastion_key_name"      { default = "bastion-ec2-key.pem" }
 variable "bastion_volume_size"   { default = 8 }
 
 # Private EC2
 variable "private_ec2_ami"           { default = "ami-0e6f2b2fa0ca704d0" }
 variable "private_ec2_instance_type" { default = "t2.medium" }
-variable "private_ec2_key_name"      { default = "./keyfair/private-ec2-key.pem" }
+variable "private_ec2_key_name"      { default = "private-ec2-key.pem" }
 variable "private_ec2_volume_size"   { default = 8 }
