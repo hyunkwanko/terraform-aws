4355896c50e0bcefeb3e466b26f36bc2de49e05b
khk <cjdshrwkd11@gmail.com>
2024-05-21 09:44:26 +0900

    k8s dev cluster

--- env/dev/ec2.tf
+++ env/dev/ec2.tf
@@ -55,17 +55,31 @@ resource "aws_instance" "private-ec2-a-1" {
     tags = {
         Name = "${var.project_name}-${var.environment}-private-ec2-a-1"
     }
+}
 
-    user_data = <<-EOF
-             #!/bin/bash
-             sudo apt-get update
-             sudo apt-get install -y nginx
-             sudo systemctl start nginx
-             sudo systemctl enable nginx
-             echo '<!doctype html>
-             <html lang="en"><h1>Prviate EC2-a-1</h1></br>
-             </html>' | sudo tee /var/www/html/index.html
-             EOF
+## Prviate EC2-a-2
+resource "aws_instance" "private-ec2-a-2" {
+    ami = "${var.private_ec2_ami}"
+    instance_type = "${var.private_ec2_instance_type}"
+    vpc_security_group_ids = [aws_security_group.private-ec2.id]
+    iam_instance_profile = aws_iam_instance_profile.private_ec2.name
+    subnet_id = aws_subnet.private-subnet-1.id
+    associate_public_ip_address = false
+    key_name = "${var.private_ec2_key_name}"
+    disable_api_termination = false
+
+    root_block_device {
+        volume_size = "${var.private_ec2_volume_size}"
+        volume_type = "gp3"
+        delete_on_termination = true
+        tags = {
+            Name = "${var.project_name}-${var.environment}-private-ec2-a-2"
+        }
+    }
+
+    tags = {
+        Name = "${var.project_name}-${var.environment}-private-ec2-a-2"
+    }
 }
 
 # Prviate EC2-c-1
@@ -91,15 +105,4 @@ resource "aws_instance" "private-ec2-c-1" {
     tags = {
         Name = "${var.project_name}-${var.environment}-private-ec2-c-1"
     }
-
-    user_data = <<-EOF
-             #!/bin/bash
-             sudo apt-get update
-             sudo apt-get install -y nginx
-             sudo systemctl start nginx
-             sudo systemctl enable nginx
-             echo '<!doctype html>
-             <html lang="en"><h1>Prviate EC2-c-1</h1></br>
-             </html>' | sudo tee /var/www/html/index.html
-             EOF
 }
