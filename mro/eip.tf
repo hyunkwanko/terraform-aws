7b0427edf489d596877c49f22fa8e822c5e79d84
hyunkwanko <cjdshrwkd11@gmail.com>
2024-07-05 10:52:25 +0900

    mro remove

--- mro/eip.tf
+++
@@ -1,9 +0,0 @@
-# Elastic IP
-## Public EC2
-resource "aws_eip" "public-ec2-eip" {
-    instance = aws_instance.public-ec2.id
-
-    tags = {
-        Name = "${var.project_name}-${var.environment}-eip"
-    }
-}
